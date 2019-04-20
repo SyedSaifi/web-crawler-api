f57f8e1df072760f1f267fc941305502e78993f2
Saifi <Syed.Saifi@emc.com>
2019-04-20 16:34:29 +0530

    Add the default Springboot project template

---
+++ src/main/java/com/example/webcrawlerapi/WebCrawlerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.webcrawlerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrawlerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCrawlerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webcrawlerapi/WebCrawlerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.webcrawlerapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebCrawlerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
